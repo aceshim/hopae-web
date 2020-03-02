c72b5815b3d49ffebc7909dcb9805f682a8d5eee
Pensivej <jshim10@illinois.edu>
2020-03-02 22:58:22 +0900

    Updates

---
+++ precache-manifest.591e40c831dc6e1a72ce7e6adad3d727.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bcee1ea69da9af7b75087148eb8217a",
+    "url": "/hopae-web/index.html"
+  },
+  {
+    "revision": "ac0333f8aec5ad40e209",
+    "url": "/hopae-web/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d560004d9f71de30e4f2",
+    "url": "/hopae-web/static/js/2.94cdebc7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/hopae-web/static/js/2.94cdebc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac0333f8aec5ad40e209",
+    "url": "/hopae-web/static/js/main.e1c33226.chunk.js"
+  },
+  {
+    "revision": "8cab1047703916ae4e61",
+    "url": "/hopae-web/static/js/runtime-main.31314682.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hopae-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
